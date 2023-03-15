5c803dcc550a2abbda6adbd12a862b8f22f56ddb
a88019401 <a88019401@yahoo.com.tw>
2023-03-15 21:48:17 +0800

    addddddd

---
+++ src/App_28.js
@@ -0,0 +1,22 @@
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import './App.css';
+import BlogStaticPage_xx from './pages/BlogsStaticPage_28';
+import HomePage_28 from './pages/HomePage_28';
+import BlogsLocalJsonPage_28 from './pages/BlogsLocalJsonPage_28';
+import BlogsNodeServerPage_28 from './pages/BlogsNodeServerPage_28';
+function App_28() {
+  return (
+    <BrowserRouter>
+    <Routes>     
+      <Route path = '/' element={HomePage_28}></Route>
+
+      <Route path = '/card_28' element={<BlogStaticPage_xx/>}></Route>
+      <Route path = '/card2_local_28' element={<BlogsLocalJsonPage_28/>}></Route>
+      <Route path = '/card2_node_28' element={<BlogsNodeServerPage_28/>}></Route>
+
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App_28;
---
+++ src/pages/HomePage_28.js
@@ -0,0 +1,10 @@
+const HomePage_28 =() =>{
+    return(
+        <h1>
+            HomePage
+        </h1>
+    )
+}
+
+
+export default HomePage_28;
