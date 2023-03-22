18bd6fc44beff015e84f4f9afec6699eb8c73f85
a88019401 <a88019401@yahoo.com.tw>
2023-03-22 20:59:54 +0800

    add somthing

--- src/App_28.js
+++ src/App_28.js
@@ -4,6 +4,7 @@ import BlogStaticPage_xx from './pages/BlogsStaticPage_28';
 import HomePage_28 from './pages/HomePage_28';
 import BlogsLocalJsonPage_28 from './pages/BlogsLocalJsonPage_28';
 import BlogsNodeServerPage_28 from './pages/BlogsNodeServerPage_28';
+import BlogsSupabasePage_28 from './pages/BlogsSupabasePage_28';
 function App_28() {
   return (
     <BrowserRouter>
@@ -13,6 +14,7 @@ function App_28() {
       <Route path = '/card_28' element={<BlogStaticPage_xx/>}></Route>
       <Route path = '/card2_local_28' element={<BlogsLocalJsonPage_28/>}></Route>
       <Route path = '/card2_node_28' element={<BlogsNodeServerPage_28/>}></Route>
+      <Route path = '/card2_supabase_28' element={<BlogsSupabasePage_28/>}></Route>
 
     </Routes>
     </BrowserRouter>
