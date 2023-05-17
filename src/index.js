6c06ad966513a409b3d2ebd0831fa85cb510080b
a88019401 <a88019401@yahoo.com.tw>
2023-05-17 20:06:54 +0800

    get menu data from supabase

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App_28 from './App_28';
-import { DemoProvider_28 } from './context/DemoContext_28';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DemoProvider_28>
     <App_28 />
-    </DemoProvider_28>
   </React.StrictMode>
 );
 
